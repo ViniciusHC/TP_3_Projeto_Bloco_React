7d53ba1cef0a31541880cebe3dbd67126e994196
Vinicius Costa <viniciusholandac1@gmail.com>
2026-02-27 08:44:55 -0300

    alteracoes para vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import Routes from "./Routes";
+import App from "./App.jsx";
+import Routes from "./Routes.jsx";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
 
 root.render(
   <>
