375296069454c3cda2012234a8a230919d05f0cd
Vinicius Costa <viniciusholandac1@gmail.com>
2026-03-01 11:12:14 -0300

    criado testes de preenchimento de envento

---
+++ cypress/e2e/Canary/canary.cy.js
@@ -0,0 +1,9 @@
+/// <reference types = "cypress"/>
+
+describe("Canarary Test", () => {
+    it ("Deve retornar True", () => {const verdadeiro = true;
+    const ehVerdade = verdadeiro == true;
+    expect(ehVerdade).to.equal(true);
+});   
+    
+})
